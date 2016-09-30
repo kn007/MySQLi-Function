54a8cace88c3e37d0af3150e9c678953268bd011
SteakOverCooked.com <oh@steakovercooked.com>
2016-09-30 22:22:19 +0100

    function_exists
    function_exists

--- mysqli_field_flags.php
+++ mysqli_field_flags.php
@@ -1,19 +1,21 @@
 <?php
 
-function mysqli_field_flags($result, $field_offset) {
-    static $flags;
-    $flags_num = mysqli_fetch_field_direct($result, $field_offset)->flags;
-    if (!isset($flags)) {
-        $flags = array();
-        $constants = get_defined_constants(true);
-        foreach ($constants['mysqli'] as $c => $n) if (preg_match('/MYSQLI_(.*)_FLAG$/', $c, $m)) if (!array_key_exists($n, $flags)) $flags[$n] = $m[1];
+if (!function_exists("mysqli_field_flags")) {
+    function mysqli_field_flags($result, $field_offset) {
+        static $flags;
+        $flags_num = mysqli_fetch_field_direct($result, $field_offset)->flags;
+        if (!isset($flags)) {
+            $flags = array();
+            $constants = get_defined_constants(true);
+            foreach ($constants['mysqli'] as $c => $n) if (preg_match('/MYSQLI_(.*)_FLAG$/', $c, $m)) if (!array_key_exists($n, $flags)) $flags[$n] = $m[1];
+        }
+        $result = array();
+        foreach ($flags as $n => $t) if ($flags_num & $n) $result[] = $t;
+        $return = implode(' ', $result);
+        $return = str_replace('PRI_KEY','PRIMARY_KEY', $return);
+        $return = strtolower($return);
+        return $return;
     }
-    $result = array();
-    foreach ($flags as $n => $t) if ($flags_num & $n) $result[] = $t;
-    $return = implode(' ', $result);
-    $return = str_replace('PRI_KEY','PRIMARY_KEY', $return);
-    $return = strtolower($return);
-    return $return;
 }
 
 ?>
